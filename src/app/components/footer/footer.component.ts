ab39f4fbcd023edd3ed09988baca317657bfa5ac
Githiomi Daniel <dangit@mcb.mu>
2023-11-04 18:27:48 +0400

    Feat: Footer design completed

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule, RouterModule, MatIconModule],
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+
+  readonly universityLogo : string = './../../../assets/images/ALUmni_logo.png';
+
+}
