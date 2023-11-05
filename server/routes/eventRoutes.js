9c3643cfda1b911522608e75e3f29d9d48f6012c
Githiomi Daniel <dangit@mcb.mu>
2023-11-05 13:14:52 +0400

    Feat: Added route validation to authentication and events

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -7,7 +7,15 @@ const eventsController = require('./../controllers/eventsController');
 
 eventRouter.get('/events', eventsController.get_all_events);
 
-eventRouter.post('/events/new', eventsController.create_new_event);
+eventRouter.post('/events/new', [
+    check('eventTitle').not().isEmpty().trim().escape().withMessage('Event Title cannot be empty.'),
+    check('eventDescription').not().isEmpty().trim().escape().withMessage('Event Description cannot be empty.'),
+    check('venue').not().isEmpty().trim().escape().withMessage('Venue cannot be empty.'),
+    check('eventCategory').not().isEmpty().trim().escape().withMessage('Event Category cannot be empty.'),
+    check('duration').not().isEmpty().isInt().trim().escape().withMessage('Role cannot be empty.'),
+    check('createdBy').not().isEmpty().isInt().trim().escape().withMessage('Created By cannot be empty.'),
+    check('atendeeLimit', 'Atendee Limit should be a number').not().isEmpty().isInt(),
+], eventsController.create_new_event);
 
 eventRouter.get('/events/:eventId', eventsController.get_event_by_id);
 
