88af4c5789792fc50ce3b97922f2f76270005079
githiomi <danielgithiomi@gmail.com>
2024-01-23 23:59:00 +0400

    Feat: Began connect form setup and config

--- src/app/components/pages/connect/connect.component.ts
+++ src/app/components/pages/connect/connect.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-connect',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, ReactiveFormsModule],
   templateUrl: './connect.component.html',
   styleUrls: ['./connect.component.css']
 })
 export class ConnectComponent {
+
+  // Dependency injection
+  
+
+  onFormSubmit(form:any) : void {
+    if (form.valid)
+      console.log("Form is valid");
+      
+  }
 
 }
