cf89b66bdc2c19ceac3188f89539771a0629be5d
githiomi <danielgithiomi@gmail.com>
2024-01-23 23:52:57 +0400

    Feat: Added the mat-card module to the connect component

--- src/app/components/pages/connect/connect.component.ts
+++ src/app/components/pages/connect/connect.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-connect',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './connect.component.html',
   styleUrls: ['./connect.component.css']
 })
